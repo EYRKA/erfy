9f3772c1ceb45df27314534a9a58fbe2eede8574
HikaruEgashira <shighten@gmail.com>
2020-03-05 02:19:22 +0900

    fix path & delete

--- src/components/Headings.tsx
+++ src/components/Headings.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 
-import "headings.scss";
+import "./headings.scss";
 
 const Heading1: FC = ({ children }) => {
   return <h1 className="heading-1">{children}</h1>;
